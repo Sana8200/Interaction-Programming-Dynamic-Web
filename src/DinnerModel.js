bcfffce39a6068b69b273f0f181f3263b25c960d
Sana <s.monhaserii@gmail.com>
2025-11-03 18:57:38 +0100

    New added files, started the assignmnets and project

---
+++ src/DinnerModel.js
@@ -0,0 +1,40 @@
+/* 
+   The Model keeps the state of the application (Application State). 
+   It is an abstract object, i.e. it knows nothing about graphics and interaction.
+*/
+export const model = {  
+    numberOfGuests: 2,
+    dishes: [],
+    currentDishId: null,  // null means "intentionally empty"
+
+    setCurrentDishId(dishId){
+        this.currentDishId= dishId;
+    },
+    
+    setNumberOfGuests(number){
+        if (!Number.isInteger(number) || number < 1){
+            throw new Error ("number of guests not a positive integer");
+        }
+        this.numberOfGuests = number;
+
+    },
+    
+    addToMenu(dishToAdd){
+        // array spread syntax exercise
+        // It sets this.dishes to a new array [   ] where we spread (...) the elements of the existing this.dishes
+        this.dishes= [...this.dishes, dishToAdd];
+    },
+
+    // filter callback exercise
+    removeFromMenu(dishToRemove){
+        function shouldWeKeepDishCB(dish){
+            // returns true if we want to keep the dish 
+            return dish.id !== dishToRemove.id;
+        }
+        this.dishes= this.dishes.filter(shouldWeKeepDishCB);
+    },
+    
+ 
+    // more methods will be added here, don't forget to separate them with comma!
+};
+
---
+++ src/vueReactiveModel.js
@@ -0,0 +1,17 @@
+import "/src/teacherFetch.js"; // protection against fetch() in infinite loops
+import { reactive } from "vue";
+
+export const reactiveModel="make a reactive object out of the model exported from DinnerModel";
+
+
+
+
+// ------ for Lab debug purposes ----------
+// making the reactive model available at the browser JavasScript Console
+window.myModel= reactiveModel;
+
+// making some example dishes available 
+import {dishesConst} from "/src/dishesConst.js";
+window.dishesConst= dishesConst;
+
+//myModel.addToMenu(dishesConst[2]); //You can test with more/different dishes
