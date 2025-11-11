6e2300aed6bb63505da63627468700ce336b56b2
Sana <144557568+Sana8200@users.noreply.github.com>
2025-11-11 15:10:26 +0100

    Modified Callbacks, Rendering, Events. Bootstrapping the app. Passing props to Views, root components

--- src/DinnerModel.js
+++ src/DinnerModel.js
@@ -8,30 +8,31 @@ export const model = {
     currentDishId: null,  // null means "intentionally empty"
 
     setCurrentDishId(dishId){
-        this.currentDishId= dishId;
+        this.currentDishId= dishId;     // Set the model current dish ID
     },
     
-    setNumberOfGuests(number){
-        if (!Number.isInteger(number) || number < 1){
+    setNumberOfGuests(number){     
+        if (!Number.isInteger(number) || number < 1){         // Only accepts integers larger than zero
             throw new Error ("number of guests not a positive integer");
         }
-        this.numberOfGuests = number;
-
+        this.numberOfGuests = number;    // Set the number of guests 
     },
     
     addToMenu(dishToAdd){
         // array spread syntax exercise
         // It sets this.dishes to a new array [   ] where we spread (...) the elements of the existing this.dishes
-        this.dishes= [...this.dishes, dishToAdd];
+        this.dishes= [...this.dishes, dishToAdd];   // Creates a new array, putting all the old dishes back in and add dishToAdd at the end 
     },
+
+
 
     // filter callback exercise
     removeFromMenu(dishToRemove){
         function shouldWeKeepDishCB(dish){
-            // returns true if we want to keep the dish 
-            return dish.id !== dishToRemove.id;
+            // We will not keep the dish that has the same id as dishToRemove if any
+            return dish.id !== dishToRemove.id;   // returns true for every dish except the one to remove 
         }
-        this.dishes= this.dishes.filter(shouldWeKeepDishCB);
+        this.dishes= this.dishes.filter(shouldWeKeepDishCB);   // filter creates a new array, keeping only the dishes which returned true
     },
     
  
--- src/reactjs/index.jsx
+++ src/reactjs/index.jsx
@@ -1,7 +1,11 @@
 import {createElement, Fragment} from "react";
+import { ReactRoot } from "./ReactRoot";
+
 window.React= {createElement:createElement, Fragment:Fragment}; // needed in the lab because it works with both React and Vue
 
 import { createRoot } from "react-dom/client";
+import { reactiveModel } from "/src/mobxReactiveModel";
 
 // mount the app in the browser page. Test at http://localhost:8080/react.html
-createRoot(document.getElementById('TODO')).render("Hello React world!");
+createRoot(document.getElementById('root')).render(<ReactRoot model={reactiveModel} />);
+// Implemented root string which is the ID of the div, getElementById will find div by this id root and then render 
