80ee5d003ac074e89ce390793b2490320c64714d
Sana <144557568+Sana8200@users.noreply.github.com>
2025-11-14 09:41:24 +0100

    implement sidebarView with layout, table, and custom event handlers

--- src/views/sidebarView.jsx
+++ src/views/sidebarView.jsx
@@ -1,3 +1,99 @@
+import { dishType, menuPrice, sortDishes } from "/src/utilities.js";
+import "/src/style.css"
+
+
 export function SidebarView(props){
-    return "SidebarView stub: number is "+props.number + " and we have "+props.dishes.length+ " dishes";
+
+    // Event handler for - button 
+    function handleMinusACB(){
+        console.log("Decreasing the number of guests:", props.number - 1);
+        props.onNumberChange(props.number - 1);
+    }
+
+    // Event handler for + button 
+    function handlePlusACB(){
+        console.log("Increasing the number of guests:", props.number + 1);
+        props.onNumberChange(props.number + 1);
+    }
+
+    return (
+        <div>
+            {/* minus button disables when the number is excatly equals to 1 */}
+            <button 
+                disabled={props.number === 1}
+                onClick={handleMinusACB}>
+                -
+            </button> 
+
+            {props.number}   
+
+            <button
+                onClick={handlePlusACB}>
+                +
+            </button>    
+            
+            {/* Dish table */}
+            <table>
+                <tbody>
+                    {   // mapping dish array to table rows after sorting 
+                        sortDishes(props.dishes)?.map(dishTableRowCB)
+                    }
+                    <tr>
+                        <td></td>
+                        <td>Total:</td>
+                        <td></td>
+                        <td className="align-right">
+                            {/* Total price, scaled by guest count */}
+                            {(menuPrice(props.dishes) * props.number).toFixed(2)}  
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+
+
+    /* Array Rendering Callback */
+    // A template for each dish row 
+    function dishTableRowCB(dish){
+        
+        // Nested handler for dish link click
+        function handleDishLinkACB(){
+            console.log("Customer selected Dish:", dish.title);
+            props.onSelectedDish(dish);
+        }
+
+        // Nested handler for X button click 
+        function handleRemoveDishACB(){
+            console.log("Customer selected Dish to Remove:", dish.title);
+            props.onSelectedRemoveDish(dish);
+        }
+
+
+        return (
+            // Use dish ID as the unique react key
+            <tr key={dish.id}>
+                <td>
+                    {/* X button for deleting a dish */}
+                    <button onClick={handleRemoveDishACB}>
+                        X
+                    </button>      
+                </td>
+                <td>
+                    {/* dish name as a hyperlink using # as href, hyperlink destination for single page application */}
+                    {/* fire a custom event, the parameter is dish as object */}
+                    <a href="#" onClick={handleDishLinkACB}>
+                        {dish.title}
+                    </a>      
+                </td>
+                <td>
+                    {dishType(dish)}    
+                </td>
+                <td className="align-right">
+                    {/* Price per dish, scaled by guest count */}
+                    {(dish.pricePerServing * props.number).toFixed(2)}       
+                </td>
+            </tr>
+        );
+    }
 }
