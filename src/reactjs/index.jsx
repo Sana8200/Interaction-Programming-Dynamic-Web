32e63abb8a86f98b311a5c02e1ee7c9a36b2a7d4
Sana <s.monhaserii@gmail.com>
2025-11-14 09:38:48 +0100

    mount reactRoot component and bootstrap model with initial dishes

--- src/reactjs/index.jsx
+++ src/reactjs/index.jsx
@@ -3,9 +3,10 @@ import { ReactRoot } from "./ReactRoot";
 
 window.React= {createElement:createElement, Fragment:Fragment}; // needed in the lab because it works with both React and Vue
 
+
 import { createRoot } from "react-dom/client";
 import { reactiveModel } from "/src/mobxReactiveModel";
 
 // mount the app in the browser page. Test at http://localhost:8080/react.html
-createRoot(document.getElementById('root')).render(<ReactRoot model={reactiveModel} />);
 // Implemented root string which is the ID of the div, getElementById will find div by this id root and then render 
+createRoot(document.getElementById('root')).render(<ReactRoot model={reactiveModel} />);
