9dcdb8581bc462a9d862b0c3f8d99741e1366830
Sana <144557568+Sana8200@users.noreply.github.com>
2025-11-14 09:40:45 +0100

    Implement ingredients table with sorting and person/persons logic

--- src/views/summaryView.jsx
+++ src/views/summaryView.jsx
@@ -1,56 +1,51 @@
-// un-comment when needed:
-//import {sortIngredients} from "/src/utilities.js";
-//import "/src/style.css"
+import {sortIngredients} from "/src/utilities.js";
+import "/src/style.css"
 
 /* Functional JSX component. Name must start with capital letter */
-export function SummaryView(props){
+export function SummaryView(props){    
     return (
             <div className="debug">
 
-  
-
-
               {/* TW 1.2 note the syntax: {JS_expression_or_comment} */}
-              Summary for <span title="nr guests">{props.people}</span> persons:
-
-
-
-
-              {/* TW 1.3: remove this line (and the TW1.3 one below) to uncomment
-
+              {/* DOM tree JSX code, Rendering the number of people */}
+              Summary for <span title="nr guests">{props.people}</span> 
+              
+              {/* Conditional Expression .? logic */}
+              {props.people === 1 ? " person" : " persons"}:      
+          
               <table>
                 <thead>
                   <tr>
                     <th>Name</th>
                     <th>Aisle</th>
                     <th>Quantity</th>
-                    <th>unit</th>
+                    <th>Unit</th>
                   </tr>
                 </thead>
+
                 <tbody>
                   { //  <---- in JSX/HTML, with this curly brace, we go back to JavaScript
                     // Here Array Rendering is used to generate a table row for each element of the ingredients prop (an array) 
-                    props.TODO?.map(ingredientTableRowCB)
+                    // Metod Chaining : We sort the ingredients array first,and then we .map() over the sorted array.
+                    sortIngredients(props.ingredients)?.map(ingredientTableRowCB)
                   }
                 </tbody>
               </table>
-
-              TW 1.3: remove this line to uncomment */} 
-
-
-
-
             </div>
     );
     
+
+
     /* callback for Array Rendering in TW 1.3 */
-    function ingredientTableRowCB(ingr){
-        return <tr key={ /* Reflect on what's a key in array rendering! */ ingr.id } >
+    // This is a template for each single row that adds the array rendering keys by setting it to the ingredients id property 
+    function ingredientTableRowCB(ingr){ 
+        return <tr key={ingr.id} >    
                  <td>{ingr.name}</td>
-                 <td>TODO aisle</td>
-                 <td className="TODO">TODO qty</td>
-                 <td> TODO unit </td>
+                 <td>{ingr.aisle}</td>
+                 <td className="align-right">
+                  {(ingr.amount * props.people).toFixed(2)}    {/* gets the total amoung of that ingredients needed for all the guests */}
+                 </td>
+                 <td>{ingr.unit}</td>
                </tr>;
     }
 }
-
