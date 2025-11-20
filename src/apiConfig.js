2ec832702a8522c573ad60c3fc02ce4af78cb134
Sana <s.monhaserii@gmail.com>
2025-11-20 17:42:43 +0100

    API configuration

---
+++ src/apiConfig.js
@@ -0,0 +1,6 @@
+
+const PROXY_URL = "https://brfenergi.se/iprog/group/795/" + "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com";
+
+const PROXY_KEY= "3d2a031b4cmsh5cd4e7b939ada54p19f679jsn9a775627d767";
+
+export { PROXY_KEY, PROXY_URL };
