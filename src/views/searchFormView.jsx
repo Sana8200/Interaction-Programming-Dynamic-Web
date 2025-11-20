7382ee1853a437a151eee306eb446d40dee686ae
Sana <144557568+Sana8200@users.noreply.github.com>
2025-11-20 17:46:26 +0100

    searchForm view is for managing search click button and searching, searchresult view managnes the results view of the search click, and suspense view is for managing the suspense display when there is nothing to show or when is loading

---
+++ src/views/searchFormView.jsx
@@ -0,0 +1,46 @@
+import "/src/style.css"
+
+
+// This Form should remeber the vlaues when the view is hidden and shown again to the user 
+export function SearchFormView(props) {
+    // props used : dishTypeOptions, text, type, onTextChange, onTypeChange, onSearchClick
+    
+    function dishTypeOptionsCB(optionStrng){
+        return <option key = {optionStrng} value = {optionStrng}>{optionStrng}</option>;
+    }
+
+    // Event Handler 
+    function handleTextChagneACB(event){
+        props.onTextChange(event.target.value);
+    }
+
+    function handleTypeChangeACB(event){
+        props.onTypeChange(event.target.value);
+    }
+
+    function handleSearchACB(event){
+        props.onSearchClick();
+    }
+
+    // someProp || "" show the prop if it's defiend, or an empty string otherwise 
+    return (
+        <div className="search-boxes">
+            <input value = {props.text || ""}
+                   onChange = {handleTextChagneACB}
+                   placeholder="🔍 Search..."/>
+
+            <select value={props.type || ""}
+                    onChange = {handleTypeChangeACB}>
+
+                <option value="">Choose:</option>
+                {props.dishTypeOptions.map(dishTypeOptionsCB)}
+            </select>
+
+            <button onClick = {handleSearchACB}>
+                Search!
+            </button>
+
+            </div>
+    );
+                     
+}
