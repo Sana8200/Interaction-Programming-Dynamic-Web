50262875678fae66c12313396cebb59386be7a09
Sana <144557568+Sana8200@users.noreply.github.com>
2025-11-20 17:49:06 +0100

    Connecting the model to the firebase, using the connectToPersistance funciton

--- src/mobxReactiveModel.js
+++ src/mobxReactiveModel.js
@@ -1,30 +1,45 @@
 import "/src/teacherFetch.js"; // protection against fetch() in infinite loops
-import { observable, configure } from "mobx";
+import { reaction, observable, configure } from "mobx";
+import {dishesConst} from "/src/dishesConst.js";
+import { model } from "/src/DinnerModel.js";    // Import the javascript model 
+
+import { connectToPersistence } from "./firestoreModel";
+
+
 configure({ enforceActions: "never", });  // we don't use Mobx actions in the Lab
 
 
-import { model } from "/src/DinnerModel.js";    // Import the javascript model 
-
-export const reactiveModel = observable(model);    // reactive model setup
 // reactive model setup uses observable from mobx to export the model (application state) to the app 
-
+export const reactiveModel = observable(model);    // reactive model setup
 
 
 // ------ for Lab debug purposes ----------
 // making the reactive model available at the browser JavasScript Console
 window.myModel= reactiveModel;
 
-// making some example dishes available 
-import {dishesConst} from "/src/dishesConst.js";
 window.dishesConst= dishesConst;
 
-//myModel.addToMenu(dishesConst[2]); //You can test with more/different dishes
-// Bootstrapping, setting up the inital state (index.js)
-myModel.addToMenu(dishesConst[0]);
-myModel.addToMenu(dishesConst[1]);
-myModel.addToMenu(dishesConst[2]);
-myModel.addToMenu(dishesConst[3]);
-myModel.addToMenu(dishesConst[4]);
-myModel.addToMenu(dishesConst[5]);
-myModel.addToMenu(dishesConst[6]);
-myModel.addToMenu(dishesConst[7]);
+
+// returns the reactive value 
+function currentDishIDACB(){
+    return reactiveModel.currentDishId;
+}
+
+// side effect function whenever the dish ID changes
+function currentDishSideEffectACB(){
+    return reactiveModel.currentDishEffect();
+}
+// wathcer reaction for watching changes in currentDishId and triggering side effect    
+reaction(currentDishIDACB, currentDishSideEffectACB);
+
+
+// runs the firestore initialization (connecting persistence), Connecting to persistence. 
+// We pass the model and the reaction funciton , allowing firestoreModel to observe changes without importing Mobs directly
+// Passing the side effect watcher ensures that our firestoreModel does not depend on your reactive object technology.
+connectToPersistence(reactiveModel, reaction);
+
+
+
+
+myModel.doSearch({});
+
