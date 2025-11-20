16574d17e24e1f9d7b9eebf3f631604e40518219
Sana <s.monhaserii@gmail.com>
2025-11-20 17:43:10 +0100

    FireBase configuration

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyDWcWu3_Vwd2Cs65hUQVL9_eRtI5Vm1CKk",
+  authDomain: "sanamo-ht25-web.firebaseapp.com",
+  projectId: "sanamo-ht25-web",
+  storageBucket: "sanamo-ht25-web.firebasestorage.app",
+  messagingSenderId: "286758178388",
+  appId: "1:286758178388:web:75531055aa9d7388e527c1",
+  measurementId: "G-ZJW0W7VQ6M"
+};
+
+
+
+// Set up a Firebase app with a Cloud firestore database, above is the unique web app configuration  
